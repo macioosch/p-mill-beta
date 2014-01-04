efb9f6b6f6537b3f64172c2924a041d92841f566
Maciej Chudak <macioosch@gmail.com>
2014-01-04 15:55:37 +0100

    comment changes in main.cpp

--- main.cpp
+++ main.cpp
@@ -12,14 +12,16 @@ int main(int argc, char *argv[])
 
     vector<ball> b;
 
-    if (1 == params.type)
+    if (1 == params.type) {
         type_1_init(params, b);
-    //else if (2 == params.type)
-    //    type_2_init(params, b);
-    //else if (3 == params.type)
-    //    type_3_init(params, b);
-
-    simulate_1(params, b);
+        simulate_1(params, b);
+    } /*else if (2 == params.type) {
+        type_2_init(params, b);
+        simulate_2(params, b);
+    } else if (3 == params.type) {
+        type_3_init(params, b);
+        simulate_3(params, b);
+    }*/
 
     return return_status;
 }
